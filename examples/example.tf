d1298f74160672de8a2092f4ff249776975bcfcd
itsdalmo <kristian@doingit.no>
2018-06-27 12:02:42 +0200

    Remove unused datasources in example.

--- examples/example.tf
+++ examples/example.tf
@@ -2,14 +2,6 @@ provider "aws" {
   region = "eu-west-1"
 }
 
-data "aws_vpc" "main" {
-  default = true
-}
-
-data "aws_subnet_ids" "main" {
-  vpc_id = "${data.aws_vpc.main.id}"
-}
-
 module "certificate" {
   source  = "../"
   domain  = "example.com"
