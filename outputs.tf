a64b6717e12b29d1e5bdc72c94478f86cae07df8
Rickard Löfström <rickard.lofstrom@gmail.com>
2018-10-10 08:25:44 +0200

    fix: return arn as string not list of arns

--- outputs.tf
+++ outputs.tf
@@ -3,10 +3,10 @@
 # ------------------------------------------------------------------------------
 output "arn" {
   description = "The ARN of the certificate that is being validated."
-  value       = "${aws_acm_certificate_validation.main.*.certificate_arn}"
+  value       = "${element(concat(aws_acm_certificate_validation.main.*.certificate_arn, list("")), 0)}"
 }
 
 output "wildcard_arn" {
   description = "The ARN of the wildcard certificate that is being validated."
-  value       = "${aws_acm_certificate_validation.wildcard.*.certificate_arn}"
+  value       = "${element(concat(aws_acm_certificate_validation.wildcard.*.certificate_arn, list("")), 0)}"
 }
