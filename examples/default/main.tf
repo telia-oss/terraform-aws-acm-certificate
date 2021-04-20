f77554b34cfd4aa5eecafa4b1e2f3811981d3862
Sadri Gargouri <74971461+SadriG91@users.noreply.github.com>
2021-04-20 10:13:22 +0200

    changed variable name certificate_name to domain_name for ease of use (#18)
    * changed certificate_name to domain_name for ease of use
    
    * changed to lowercase sans
    
    Co-authored-by: Lars Tobias Skjong-Børsting <larstobi@relatime.no>

--- examples/default/main.tf
+++ examples/default/main.tf
@@ -9,7 +9,21 @@ provider "aws" {
 module "certificate" {
   source           = "../../"
   hosted_zone_name = "<route53-zone-name>"
-  certificate_name = "default-test.<route53-zone-name>"
+  domain_name      = "default-test.<route53-zone-name>"
+
+
+  tags = {
+    environment = "dev"
+    terraform   = "True"
+  }
+}
+
+module "certificate_with_sans" {
+  source                    = "../../"
+  hosted_zone_name          = "<route53-zone-name>"
+  domain_name               = "default-test-1.<route53-zone-name>"
+  subject_alternative_names = ["default-test-2.<route53-zone-name>", "default-test-3.<route53-zone-name>"]
+
 
 
   tags = {
@@ -35,6 +49,6 @@ output "certificate_arn" {
   value = module.certificate.arn
 }
 
-output "certificate_with_SANs_arn" {
-  value = module.certificate_with_SANs.arn
+output "certificate_with_sans_arn" {
+  value = module.certificate_with_sans.arn
 }
